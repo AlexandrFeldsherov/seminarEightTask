5379d6281f4a9edde0d31076b90664445dc50d2b
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-07-24 22:53:12 +0300

    v001

--- task054/Program.cs
+++ task054/Program.cs
@@ -1,4 +1,5 @@
-﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+﻿/*Задача 54: Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+каждой строки двумерного массива.
 Например, задан массив:
 1 4 7 2
 5 9 2 3
@@ -7,3 +8,64 @@
 7 4 2 1
 9 5 3 2
 8 4 4 2*/
+int[,] NewMatrixArrRand(int a, int b)//создает массив заполнненный псевдослучайными вещественными числами числами в интервале от 'a' до 'b'
+{
+    Random rnd = new Random();
+    int line = rnd.Next(3, 5); //случайная размерность строк массива
+    int column = rnd.Next(3, 5); //случайная размерность сстолбцов массива
+    int[,] newArr = new int[line, column];
+
+    for (int i = 0; i < line; i++)
+    {
+        for (int ind = 0; ind < column; ind++)
+        {
+            int value = rnd.Next(a, b);
+            newArr[i, ind] = value;
+        }
+    }
+    return newArr;
+}
+int[,] ArraySortingLines(int[,] array)
+{
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    int max = 0;
+                    max = array[i, j + 1];
+                    array[i, j + 1] = array[i, j];
+                    array[i, j] = max;
+                }
+            }
+        }
+    }
+    return array;
+}
+void MatrixPrintArray(int[,] array)// печать двумерного массива int, в виде таблицы
+{
+
+    for (int ind = 0; ind < array.GetLength(0); ind++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            if (i == array.GetLength(1) - 1) Console.WriteLine($" {array[ind, i],6}");
+            else Console.Write($"{array[ind, i],6}");
+        }
+
+    }
+}
+Console.Clear();
+int[,] array = NewMatrixArrRand(1, 15);
+Console.WriteLine();
+MatrixPrintArray(array);
+Console.WriteLine();
+array = ArraySortingLines(array);
+Console.WriteLine();
+MatrixPrintArray(array);
+
--- task062/Program.cs
+++ task062/Program.cs
@@ -4,3 +4,85 @@
 12 13 14 05
 11 16 15 06
 10 09 08 07*/
+
+bool OutPosition(int[,] arr, int line, int column)//есть ли индекс в массиве
+{
+    return (line < arr.GetLength(0)
+                     && column < arr.GetLength(1)
+                        && line >= 0
+                            && column >= 0);
+}
+
+int[,] ArrayСompletionSnake(int[,] array, int i, int j) /*заполнение пустого int[,] змейкой с 
+определенной точки принадлежащей массиву*/
+{
+    int lastValueArray = array[i, j] + 1;
+    if (array[i, j] == 0)
+    {
+        array[i, j] += 1;
+    }
+    else if (OutPosition(array, i, j + 1) && array[i, j + 1] == 0) j = j + 1;
+    else if (OutPosition(array, i + 1, j) && array[i + 1, j] == 0) i = i + 1;
+    else if (OutPosition(array, i, j - 1) && array[i, j - 1] == 0) j = j - 1;
+    else if (OutPosition(array, i - 1, j) && array[i - 1, j] == 0) i = i - 1;
+    else return array;
+    array[i, j] = lastValueArray;
+    array = ArrayСompletionSnake(array, i, j);
+    return array;
+}
+int[,] ArrayСompletionSpiral(int[,] array, int i, int j) /*заполнение пустого int[,] спираль с 
+определенной точки принадлежащей массиву*/
+{
+    if (array[i, j] == 0)
+    {
+        if (OutPosition(array, i, j - 1)) array[i, j] += array[i, j - 1] + 1;
+        else array[i, j] += 1;
+    }
+    else return array;
+    while (OutPosition(array, i, j + 1) && array[i, j + 1] == 0)
+    {
+        int lastValueArray = array[i, j] + 1;
+        j++;
+        array[i, j] = lastValueArray;
+    }
+    while (OutPosition(array, i + 1, j) && array[i + 1, j] == 0)
+    {
+        int lastValueArray = array[i, j] + 1;
+        i++;
+        array[i, j] = lastValueArray;
+    }
+    while (OutPosition(array, i, j - 1) && array[i, j - 1] == 0)
+    {
+        int lastValueArray = array[i, j] + 1;
+        j--;
+        array[i, j] = lastValueArray;
+    }
+    while (OutPosition(array, i - 1, j) && array[i - 1, j] == 0)
+    {
+        int lastValueArray = array[i, j] + 1;
+        i--;
+        array[i, j] = lastValueArray;
+    }
+    j++;
+    array = ArrayСompletionSpiral(array, i, j);
+    return array;
+}
+
+
+void PrintMatrixArray(int[,] array)// печать двумерного массива int в виде таблицы
+{
+
+    for (int ind = 0; ind < array.GetLength(0); ind++)
+    {
+        for (int i = 0; i < array.GetLength(1); i++)
+        {
+            if (i == array.GetLength(1) - 1) Console.WriteLine($" {array[ind, i],2}");
+            else Console.Write($"{array[ind, i],3}");
+        }
+
+    }
+}
+int[,] array = new int[4, 4];
+array = ArrayСompletionSpiral(array, 0, 0);
+//array = ArrayСompletionSnake(array, 0, 0);
+PrintMatrixArray(array);
