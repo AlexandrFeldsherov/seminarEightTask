ba1288469ab549fdb5d79fdb13e4ef35c5b7b0e9
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-08-14 14:26:33 +0300

    vb2

--- butc/Program.cs
+++ butc/Program.cs
@@ -1,22 +1,32 @@
-﻿int sizeArray = 5;
-int[] array = new int[sizeArray];
-Console.WriteLine("Введите элемент массива : ");
-for (int i = 0; i < sizeArray; i++)
+﻿int[] NewArrayConsole(int[] array)
 {
-    array[i] = Convert.ToInt32(Console.ReadLine());
-}
-for (int i = 0; i < sizeArray - 1; i++)
-{
-    int min = i;
-    for (int j = i + 1; j < sizeArray; j++)
+    Console.WriteLine("Введите элемент массива : ");
+    for (int i = 0; i < array.Length; i++)
     {
-        if (array[min] > array[j])
-        {
-            min = j;
-        }
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
-    int value = array[i];
-    array[i] = array[min];
-    array[min] = value;
+    return array;
 }
-Console.WriteLine(string.Join(", ", array));
+void ArraySortingChoice(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int min = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[min] > array[j])
+            {
+                min = j;
+            }
+        }
+        int value = array[i];
+        array[i] = array[min];
+        array[min] = value;
+    }
+    Console.WriteLine(string.Join(", ", array));
+}
+Console.WriteLine("Введите кл-во элементов массива : ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[sizeArray];
+array = NewArrayConsole(array);
+ArraySortingChoice(array);
