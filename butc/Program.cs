71a56d4fcd66fbc075e8cebd6980142c37909ecd
[AlexandrFeldsherov] <[alexandrfeldsherov@mail.ru]>
2022-08-14 14:08:36 +0300

    vb1

---
+++ butc/Program.cs
@@ -0,0 +1,22 @@
+﻿int sizeArray = 5;
+int[] array = new int[sizeArray];
+Console.WriteLine("Введите элемент массива : ");
+for (int i = 0; i < sizeArray; i++)
+{
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < sizeArray - 1; i++)
+{
+    int min = i;
+    for (int j = i + 1; j < sizeArray; j++)
+    {
+        if (array[min] > array[j])
+        {
+            min = j;
+        }
+    }
+    int value = array[i];
+    array[i] = array[min];
+    array[min] = value;
+}
+Console.WriteLine(string.Join(", ", array));
